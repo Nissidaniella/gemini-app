83638df159f3302c8dd3fa052c00f3b28648e7c1
Nissi Daniella Diallo <nissidaniella@yahoo.com>
2024-11-27 14:42:52 +0000

    first commit

---
+++ src/components/Vision.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import "../index.css";
+
+export default function vision() {
+  return (
+    <>
+      <div className="vision-container">
+        <div className="vision-text">
+            <h1> Our <span className='big'>Vision</span> </h1>
+            <p>
+            Our vision is a world where individuals and communities are empowered to drive positive change through access to essential resources like education and healthcare. We are committed to sustainable development and social justice, advancing these goals through collaborative, community-led initiatives that tackle pressing global challenges and promote equity and opportunity for all.</p>
+        </div>
+
+        <div className="vision-box">
+          <div className="box"> 
+
+
+
+<div class="card-container">
+  <div class="card">
+  <div class="front-content">
+  <h1>Organizational <br></br>Culture
+</h1>
+<p>• Leadership by example   
+• Integrity<br></br>
+• Positive and productive work environment
+• Innovation <br></br>
+• Excellence</p>
+  </div>
+  <div class="content">
+
+  </div>
+</div>
+</div>
+
+<div class="card-container">
+  <div class="card">
+  <div class="front-content">
+  <h1>Core <br></br> Values
+  </h1>
+  <p>• Empowerment
+  • Collaboration
+<br></br>
+• Equity 
+• Accountability<br></br>
+• Innovation </p>
+  </div>
+  <div class="content">
+
+  </div>
+</div>
+</div>
+
+
+
+<div class="card-container">
+  <div class="card">
+  <div class="front-content">
+  <h1>Organizational Structure
+  </h1>
+  <p>• Board of Directors
+  • Chief Executive Officer
+  •  Program Directors
+  • Administrative Staff
+  • Development Staff
+  • Communications Staff
+  • Program Staff<br></br>
+   •  Volunteers</p>
+  </div>
+  <div class="content">
+
+  </div>
+</div>
+</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "../index.css";
+import Navbar from "../components/Navbar";
+import Gemini from '../components/Gemini';
+import Who from "../components/Who";
+import Vision from "../components/Vision";
+import Mission from "../components/Mission";
+import Target from "../components/Target";
+import Earl from '../components/Earl';
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <Gemini/>
+      <Who />
+      <Vision />
+      <Mission />
+      <Target />
+      <Earl/>
+    </div>
+  );
+}
